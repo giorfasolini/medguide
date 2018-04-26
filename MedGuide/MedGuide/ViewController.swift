983e56a80d1e0ea5a07d6f969957459806e66fb3
giorfasolini <giorfasolini123@gmail.com>
2018-04-26 16:31:34 +0700

    Add files via upload

---
+++ MedGuide/MedGuide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MedGuide
+//
+//  Created by Gior Fasolini on 25/04/18.
+//  Copyright © 2018 Gior Fasolini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
