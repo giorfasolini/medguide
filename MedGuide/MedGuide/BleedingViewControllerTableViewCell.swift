88b4aaea3c05f85eda2039d7eb9151d02bfb48dd
Unknown <christian.limansyah@gmail.com>
2018-04-30 14:13:38 +0700

    Christian nambahin isi konten di bleeding (Belom di segue)

---
+++ MedGuide/MedGuide/BleedingViewControllerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BleedingViewControllerTableViewCell.swift
+//  MedGuide
+//
+//  Created by Christian Limansyah on 30/04/18.
+//  Copyright © 2018 Gior Fasolini. All rights reserved.
+//
+
+import UIKit
+
+class BleedingViewControllerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
